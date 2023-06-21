3bf499ce36b70fdfded70c58b12466d345bd09e4
Joar Christoffersen <jc@fjellsport.no>
2023-06-21 18:38:55 +0200

    added width to img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <img src="mg-logo.png" alt="logo" width={200} height={100} />
+          <img src="mg-logo.png" alt="logo" width={180} height={100} />
         </Link>
         <div className={`m-pages ${showMenu ? 'show-menu' : ''}`}>
           <Link to="/training-plan">
